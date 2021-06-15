c57f25afbbd3356f2364cd6fd685fbd9dc31062e
dav3rid <davecodingstuff@gmail.com>
2021-06-15 09:35:23 +0100

    remove trailing commas from users dev + test

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,31 +2,37 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    avatar_url:
+      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
-  },
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
+  }
 ];
--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -2,22 +2,23 @@ module.exports = [
   {
     username: 'butter_bridge',
     name: 'jonny',
-    avatar_url: 'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
+    avatar_url:
+      'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg'
   },
   {
     username: 'icellusedkars',
     name: 'sam',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4'
   },
   {
     username: 'rogersop',
     name: 'paul',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4'
   },
   {
     username: 'lurker',
     name: 'do_nothing',
     avatar_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-  },
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png'
+  }
 ];
