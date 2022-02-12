18d2b074f238449b6969f9f084c0c820577ec0d5
daviidbartlett <daviidbartlett@gmail.com>
2022-02-12 12:10:33 +0000

    remove test file template

--- __tests__/app.test.js
+++
@@ -1,6 +0,0 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const  seed  = require('../db/seeds/seed.js');
-
-beforeEach(() => seed(testData));
-afterAll(() => db.end());
