c5b84bbd09e1cf783a019374749d873d88b0629d
majorhaji <48958523+majorhaji@users.noreply.github.com>
2022-12-13 14:46:32 +0000

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 const app = require("../app");
 const db = require("../db/connection");
-const { expect } = require("@jest/globals");
+
 
 require("jest-sorted");
 
