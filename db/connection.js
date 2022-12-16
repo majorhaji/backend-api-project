765c5aa3054f9bdea2daf1886819a17af3bb09fc
Yusuf <yusufhaji1943@gmail.com>
2022-12-16 14:19:45 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,17 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+const config =
+  ENV === "production"
+    ? { connectionString: process.env.DATABASE_URL, max: 2 }
+    : {};
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
