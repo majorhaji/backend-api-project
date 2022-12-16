2425510947202a0723e2e2b412e4c770236d5f07
Yusuf <yusufhaji1943@gmail.com>
2022-12-16 14:35:40 +0000

    tables on elephantsql

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,12 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 const config =
   ENV === "production"
     ? { connectionString: process.env.DATABASE_URL, max: 2 }
     : {};
-
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
